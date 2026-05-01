a55222db666723192dc29ac9e7bf7b26eea135d7
Arup3201 <arupjana7365@gmail.com>
2026-05-01 18:26:58 +0530

    update: no option support

--- main.go
+++ main.go
@@ -14,99 +14,97 @@ func main() {
 		Usage: wc -c -m -w -l FILE
 	*/
 
-	if len(os.Args) >= 3 {
-		var options []string
-		var filename string
+	var options []string
+	var filename string
 
-		options = os.Args[1 : len(os.Args)-1]
-		filename = os.Args[len(os.Args)-1]
+	options = os.Args[1 : len(os.Args)-1]
+	filename = os.Args[len(os.Args)-1]
 
-		var f *os.File
-		var err error
-		f, err = os.OpenFile(filename, os.O_RDONLY, os.ModePerm)
-		if err != nil {
-			fmt.Printf("Error while opening file: %s\n", err)
-			return
-		}
-		defer f.Close()
+	var f *os.File
+	var err error
+	f, err = os.OpenFile(filename, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Error while opening file: %s\n", err)
+		return
+	}
+	defer f.Close()
 
-		_, err = f.Seek(0, io.SeekStart)
-		if err != nil {
-			fmt.Printf("Error encountered while starting: %s\n", err)
-			return
-		}
+	_, err = f.Seek(0, io.SeekStart)
+	if err != nil {
+		fmt.Printf("Error encountered while starting: %s\n", err)
+		return
+	}
 
-		var container = make([]byte, 1)
-		var bytesCnt,
-			charsCnt,
-			wordsCnt,
-			linesCnt uint64
-		var ch []byte // single or multi-byte character (UTF-8)
-		var prevByte byte = 0
-		var SPACE_BYTES = []byte{
-			'\t',
-			'\n',
-			'\v',
-			'\f',
-			'\r',
-			' ',
-			133, // NEW LINE
-			160, // NO-BREAK SPACE
-		}
-		for {
-			_, err = f.Read(container)
-			if err == io.EOF {
-				if prevByte != 0 && !bytes.Contains(
-					SPACE_BYTES,
-					[]byte{prevByte},
-				) {
-					wordsCnt += 1
-				}
-
-				break
-			} else if err != nil {
-				fmt.Printf("Error encountered while scanning the file: %s\n", err)
-				return
-			}
-
-			bytesCnt += 1
-
-			ch = append(ch, container[0])
-			if utf8.FullRune(ch) {
-				charsCnt += 1
-				ch = []byte{}
-			}
-
+	var container = make([]byte, 1)
+	var bytesCnt,
+		charsCnt,
+		wordsCnt,
+		linesCnt uint64
+	var ch []byte // single or multi-byte character (UTF-8)
+	var prevByte byte = 0
+	var SPACE_BYTES = []byte{
+		'\t',
+		'\n',
+		'\v',
+		'\f',
+		'\r',
+		' ',
+		133, // NEW LINE
+		160, // NO-BREAK SPACE
+	}
+	for {
+		_, err = f.Read(container)
+		if err == io.EOF {
 			if prevByte != 0 && !bytes.Contains(
 				SPACE_BYTES,
 				[]byte{prevByte},
-			) && bytes.Contains(
-				SPACE_BYTES,
-				container,
 			) {
 				wordsCnt += 1
 			}
 
-			if container[0] == '\n' {
-				linesCnt += 1
-			}
-
-			prevByte = container[0]
+			break
+		} else if err != nil {
+			fmt.Printf("Error encountered while scanning the file: %s\n", err)
+			return
 		}
 
-		if slices.Contains(options, "-c") {
-			fmt.Printf("%d ", bytesCnt)
-		}
-		if slices.Contains(options, "-m") {
-			fmt.Printf("%d ", charsCnt)
-		}
-		if slices.Contains(options, "-w") {
-			fmt.Printf("%d ", wordsCnt)
-		}
-		if slices.Contains(options, "-l") {
-			fmt.Printf("%d ", linesCnt)
+		bytesCnt += 1
+
+		ch = append(ch, container[0])
+		if utf8.FullRune(ch) {
+			charsCnt += 1
+			ch = []byte{}
 		}
 
-		fmt.Printf("%s\n", filename)
+		if prevByte != 0 && !bytes.Contains(
+			SPACE_BYTES,
+			[]byte{prevByte},
+		) && bytes.Contains(
+			SPACE_BYTES,
+			container,
+		) {
+			wordsCnt += 1
+		}
+
+		if container[0] == '\n' {
+			linesCnt += 1
+		}
+
+		prevByte = container[0]
 	}
+
+	if len(options) == 0 || slices.Contains(options, "-c") {
+		fmt.Printf("c:%d ", bytesCnt)
+	}
+	if slices.Contains(options, "-m") {
+		fmt.Printf("m:%d ", charsCnt)
+	}
+	if len(options) == 0 || slices.Contains(options, "-w") {
+		fmt.Printf("w:%d ", wordsCnt)
+	}
+	if len(options) == 0 || slices.Contains(options, "-l") {
+		fmt.Printf("l:%d ", linesCnt)
+	}
+
+	fmt.Printf("%s\n", filename)
 }
